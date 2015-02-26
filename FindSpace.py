ecf33fe1a37d42d4e6ce5464480f49a39a0ce5cc
Kevin Stowe <kevincstowe@gmail.com>
2015-02-25 18:44:09 -0700

    Added minsize to FindSpace.py

--- FindSpace.py
+++ FindSpace.py
@@ -2,28 +2,39 @@ import sys
 import getopt
 import os
 
-def get_size(start_path = '.'):
+def get_size(start_path = '.', minsize=0):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
         for f in filenames:
             fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return str(total_size*1./1000000) + " MB"
+            if os.path.exists(fp):
+                total_size += os.path.getsize(fp)
+    if (total_size*1./1000000) > minsize:
+        return str(total_size*1./1000000) + " MB"
+    else:
+        return None
+
 
 #Main method as suggested by van Rossum, simplified
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     try:
-        opts, args = getopt.getopt(argv[1:], "h", ["help"])
+        opts, args = getopt.getopt(argv[1:], "hm:", ["help", "minsize="])
 
     except:
         print "Error in args : " + str(argv[1:])
         return 2
 
+    for o, a in opts:
+        if o in ["-m", "--minsize"]:
+            minsize = int(a)
+
     if len(args) > 0:
-        for arg in args:
-            print arg + " : " + get_size(arg)
+        for arg in args:         
+            size = get_size(arg, minsize)
+            if size:
+                print arg + " : " + size
     else:
         print "Need at least one arg..."
         return 1
