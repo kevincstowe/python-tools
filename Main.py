8b2b3e9b7ccaf166046fc2e99e410f290fb903e9
Kevin Stowe <kevincstowe@gmail.com>
2015-02-25 18:35:11 -0700

    Fixed comment in Main.py

--- Main.py
+++ Main.py
@@ -7,11 +7,13 @@ def main(argv=None):
         argv = sys.argv
     try:
         opts, args = getopt.getopt(argv[1:], "h", ["help"])
-        #Functional code goes here
-
     except:
         print "Error in args : " + str(argv[1:])
         return 2
+
+    #Use opts and args here
+    # ....
+
 
 if __name__ == "__main__":
     sys.exit(main())
